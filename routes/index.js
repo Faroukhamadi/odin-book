7c3977fe8ec87575b793ca15e525291aa4b6e761
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-04-15 03:26:27 +0100

    add navbar

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // TODO: If this doesn't work change it to locals thing
-  if (req.user) res.render('home');
-  else res.redirect('/auth');
+  // if (req.user) res.render('home');
+  // else res.redirect('/auth');
+  res.render('home');
 });
 
 module.exports = router;
