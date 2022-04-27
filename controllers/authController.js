42f7e8a13e9ab1d74d69b6bba74039b59b14e6a1
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-04-27 04:46:38 +0100

    fix signup and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,27 +6,37 @@ const { format, parseISO } = require('date-fns');
 exports.local_signup_get = (req, res) => {
   res.render('signup');
 };
+
 exports.local_signup_post = (req, res, next) => {
+  console.log(
+    'birthday:',
+    '0' + req.body.month + '/' + req.body.day + '/' + req.body.year
+  );
+  console.log(req.body.gender);
+  console.log('hello');
   bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
     if (err) return next(err);
     const user = new User({
       facebook_id: '',
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
       email: req.body.email,
       hometown: req.body.hometown,
       gender: req.body.gender,
       picture:
         'https://scontent.ftun16-1.fna.fbcdn.net/v/t1.30497-1/84628273_176159830277856_972693363922829312_n.jpg?stp=c59.0.200.200a_dst-jpg_p200x200&_nc_cat=1&ccb=1-5&_nc_sid=12b3be&_nc_ohc=v46NUOVFoFgAX8HwWaV&_nc_ht=scontent.ftun16-1.fna&edm=AP4hL3IEAAAA&oh=00_AT-iZttniIR8os5NzZBzawNpdOCn3c-c7tD9ukbget_p8A&oe=62794C99',
-      posts: [],
-      friends: [],
-      friendRequests: [],
-      birthday:
-        typeof req.body.birthday === 'string'
-          ? format(parseISO(req.body.birthday), 'MM/dd/yyyy')
-          : format(req.body.birthday, 'MM/dd/yyyy'),
+      // posts: [],
+      // friends: [],
+      // friendRequests: [],
+      birthday: '0' + req.body.month + '/' + req.body.day + '/' + req.body.year,
+      // typeof req.body.birthday === 'string'
+      //   ? format(parseISO(req.body.birthday), 'MM/dd/yyyy')
+      //   : format(req.body.birthday, 'MM/dd/yyyy'),
       password: hashedPassword,
-    }).save((err) => {
+    });
+    user.save((err) => {
       if (err) return next(err);
-      res.redirect('/local');
+      res.redirect('/auth');
     });
   });
 };
@@ -49,8 +59,8 @@ exports.facebook_login_redirect = passport.authenticate('facebook', {
 });
 
 exports.local_login_post = passport.authenticate('local', {
-  successRedirect: '/success',
-  failureRedirect: '/failure',
+  successRedirect: '/',
+  failureRedirect: '/auth',
 });
 
 exports.logout = (req, res) => {
